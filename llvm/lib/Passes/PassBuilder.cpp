4995c1c4de59cf1b20a3c8e94e41271a680f2b67
Jinghao Jia <jinghao7@illinois.edu>
2022-10-22 02:55:59 -0500

    Migrate to new pass manager
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -238,6 +238,7 @@
 #include "llvm/Transforms/IPO/StripDeadPrototypes.h"
 #include "llvm/Transforms/IPO/StripSymbols.h"
 #include "llvm/Transforms/IPO/WholeProgramDevirt.h"
+#include "llvm/Transforms/InnerUnikernels/IUInsertEntry.h"
 #include "llvm/Transforms/InstCombine/InstCombine.h"
 #include "llvm/Transforms/Instrumentation/AddressSanitizer.h"
 #include "llvm/Transforms/Instrumentation/AllocToken.h"
