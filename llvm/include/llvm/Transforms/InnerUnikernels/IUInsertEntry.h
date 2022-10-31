4967df0d99d614ebc79e94d4c07c21c690934888
Jinghao Jia <jinghao7@illinois.edu>
2022-10-31 06:31:17 -0500

    entry code insertion v0
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- llvm/include/llvm/Transforms/InnerUnikernels/IUInsertEntry.h
+++ llvm/include/llvm/Transforms/InnerUnikernels/IUInsertEntry.h
@@ -1,6 +1,7 @@
 #ifndef LLVM_TRANSFORMS_IUINSERTENTRY_H
 #define LLVM_TRANSFORMS_IUINSERTENTRY_H
 
+#include "llvm/ADT/StringSet.h"
 #include "llvm/IR/PassManager.h"
 
 namespace llvm {
@@ -9,11 +10,21 @@ class Module;
 
 /// Pass to insert entry points for inner0unikernel programs
 class IUEntryInsertion : public PassInfoMixin<IUEntryInsertion> {
+  StringSet<> Sections = {
+    "tracepoint",
+  };
+
+  bool runOnModule(Module &M);
+  void insertEntry(LLVMContext &, Module &, FunctionCallee &, GlobalVariable *,
+                   Type *, StringRef);
+  void setIUFnAttr(LLVMContext &, Function *);
+
 public:
-	/// Perform insertion on the Main module
-	PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+  /// Perform insertion on the Main module
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+
 };
 
-}
+} // namespace llvm
 
 #endif /* LLVM_TRANSFORMS_IUINSERTENTRY_H */
