rex-rs/llvm-project

commit 30c0b679f459100a3cae50df9816ca08843bcbf7

entry code insertion v0
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

llvm/lib/Transforms/InnerUnikernels/IUInsertEntry.cpp
@@ -1,10 +1,31 @@
 #include "llvm/ADT/Statistic.h"
-#include "llvm/IR/Function.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/ADT/StringSet.h"
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/GlobalVariable.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/Pass.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/InnerUnikernels/IUInsertEntry.h"
+
+#include <sstream>
 #include <string>
+
+extern "C" {
+
+#include <linux/bpf.h>
+
+}
 
 using namespace llvm;
 
@@ -12,8 +33,134 @@ using namespace llvm;
 
 STATISTIC(NumInserted,  "Number of entry function inserted");
 
+void IUEntryInsertion::insertEntry(LLVMContext &C, Module &M,
+  FunctionCallee &ProgRun, GlobalVariable *ProgObj, Type *CtxPT,
+  StringRef EntryName) {
+
+  // Argument and return type
+  auto *EntryRetty = Type::getInt32Ty(C);
+  Type *EntryArgTys[1] = { CtxPT };
+
+  // Declare the function in module
+  auto *EntryTy = FunctionType::get(EntryRetty, EntryArgTys, false);
+  auto Entry = M.getOrInsertFunction(EntryName, EntryTy);
+
+  // Setup attributes
+  auto *EntryFn = cast<Function>(Entry.getCallee());
+  setIUFnAttr(C, EntryFn);
+
+  // Construct function body, starting with entry BB
+  auto *EntryBB = BasicBlock::Create(C, "start", EntryFn);\
+  IRBuilder<> InstBuilder(EntryBB);
+
+  // Bitcast away the packed attribute
+  auto *SelfType = ProgRun.getFunctionType()->getParamType(0);
+  auto *SelfObj = InstBuilder.CreateBitCast(ProgObj, SelfType);
+
+  // Construct call to prog_run
+  Value *ProgRunArgs[2] = { SelfObj, EntryFn->getArg(0) };
+  auto *ProgRunCI = InstBuilder.CreateCall(ProgRun.getFunctionType(),
+                                                      ProgRun.getCallee(),
+                                                      ProgRunArgs);
+
+  // Return
+  InstBuilder.CreateRet(ProgRunCI);
+
+  NumInserted++;
+}
+
+void IUEntryInsertion::setIUFnAttr(LLVMContext &C, Function *F) {
+  std::stringstream TargetFeatureSs;
+  TargetFeatureSs << "-avx," << "-avx2," << "-sse," << "-sse2," << "-sse3,"
+                  << "-sse4.1," << "-sse4.2," << "-crc32," << "-sse4a,"
+                  << "-ssse3," << "-avx," << "-avx2," << "-sse," << "-sse2,"
+                  << "-sse3," << "-sse4.1," << "-sse4.2," << "-crc32,"
+                  << "-sse4a," << "-ssse3";
+
+  auto AS = F->getAttributes();
+  AS = AS.addFnAttribute(C, Attribute::AttrKind::NoRedZone)
+         .addFnAttribute(C, Attribute::AttrKind::NoUnwind)
+         .addFnAttribute(C, Attribute::AttrKind::NonLazyBind)
+         .addFnAttribute(C, "probe-stack", "__rust_probestack")
+         .addFnAttribute(C, "target-cpu", "x86-64")
+         .addFnAttribute(C, "target-features", TargetFeatureSs.str())
+         .addFnAttribute(C, "tune-cpu", "generic");
+  F->setAttributes(AS);
+}
+
+bool IUEntryInsertion::runOnModule(Module &M) {
+  bool Changed = false;
+  auto &C = M.getContext();
+
+  // Iterate over all Global variables
+  for (auto &G: M.globals()) {
+    if (G.hasSection() && Sections.contains(G.getSection())) {
+      assert(G.hasInitializer() && "It really should have an initializer");
+
+      auto *Init = G.getInitializer();
+      auto *CS = cast<ConstantStruct>(Init);
+
+      // rtti
+      auto *OP0 = CS->getOperand(0);
+      auto *CDA = cast<ConstantDataArray>(OP0);
+      const auto *RawRTTI = CDA->getRawDataValues().data();
+      auto RTTI = *reinterpret_cast<const int *>(RawRTTI);
+
+      std::string ProgRunName;
+      switch (RTTI) {
+      case BPF_PROG_TYPE_TRACEPOINT:
+        ProgRunName = "_RNvXs_NtNtCs55A4jmpZCjI_18inner_unikernel_rt10tracepoint7tp_implNtB4_10tracepointNtNtB8_9prog_type9prog_type8prog_run";
+        break;
+      default:
+        llvm_unreachable("Unknown program type");
+      }
+
+      // prog_fn
+      auto *OP1 = CS->getOperand(1);
+      auto *I = cast<ConstantExpr>(OP1)->getAsInstruction();
+      auto *BCI = cast<BitCastInst>(I);
+
+      auto *ST = BCI->getSrcTy();
+      auto *PointeeT = cast<PointerType>(ST)->getNonOpaquePointerElementType();
+
+      auto *FT = cast<FunctionType>(PointeeT);
+      auto *SelfT = FT->getParamType(0);
+
+      SmallVector<Type *, 0> CtxTTys;
+      auto *CtxPT = StructType::create(C, CtxTTys)->getPointerTo();
+
+      Type *ProgRunArgTys[2] = { SelfT, CtxPT };
+      auto *ProgRunRetty = Type::getInt32Ty(C);
+
+      auto *ProgRunTy = FunctionType::get(ProgRunRetty, ProgRunArgTys, false);
+
+      auto ProgRun = M.getOrInsertFunction(ProgRunName, ProgRunTy);
+      auto *ProgRunFn = cast<Function>(ProgRun.getCallee());
+
+      setIUFnAttr(C, ProgRunFn);
+
+      // name: &'a str
+      auto *OP2 = CS->getOperand(2);
+      auto *OP2CE = cast<ConstantExpr>(OP2);
+      auto *GEPI = cast<GetElementPtrInst>(OP2CE->getAsInstruction());
+
+      auto *ProgNameInit = cast<GlobalVariable>(GEPI->getPointerOperand())->getInitializer();
+      auto *ProgNameStruct = cast<ConstantStruct>(ProgNameInit);
+      auto *ProgNameCda = cast<ConstantDataArray>(ProgNameStruct->getOperand(0));
+      std::string ProgName(ProgNameCda->getRawDataValues().data(),
+                           ProgNameCda->getType()->getNumElements());
+
+      insertEntry(C, M, ProgRun, &G, CtxPT, ProgName);
+      Changed = true;
+    }
+  }
+
+  return Changed;
+}
+
 PreservedAnalyses IUEntryInsertion::run(Module &M,
                                         ModuleAnalysisManager &AM) {
+  runOnModule(M);
   // Invalidate all analysis
   return PreservedAnalyses::none();
 }

llvm/include/llvm/Transforms/InnerUnikernels/IUInsertEntry.h
@@ -1,6 +1,7 @@
 #ifndef LLVM_TRANSFORMS_IUINSERTENTRY_H
 #define LLVM_TRANSFORMS_IUINSERTENTRY_H
 
+#include "llvm/ADT/StringSet.h"
 #include "llvm/IR/PassManager.h"
 
 namespace llvm {
@@ -9,11 +10,21 @@ class Module;
 
 /// Pass to insert entry points for inner0unikernel programs
 class IUEntryInsertion : public PassInfoMixin<IUEntryInsertion> {
+  StringSet<> Sections = {
+    "tracepoint",
+  };
+
+  bool runOnModule(Module &M);
+  void insertEntry(LLVMContext &, Module &, FunctionCallee &, GlobalVariable *,
+                   Type *, StringRef);
+  void setIUFnAttr(LLVMContext &, Function *);
+
 public:
-	/// Perform insertion on the Main module
-	PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+  /// Perform insertion on the Main module
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+
 };
 
-}
+} // namespace llvm
 
 #endif /* LLVM_TRANSFORMS_IUINSERTENTRY_H */

commit b59e4db3c52942991dd074de491de7ca263a6971

Use StructType::get() to avoid creating new types
The StructType::create() used by the old implementation creates a new
unique struct type everytime it is called. This makes
Module::getOrInsertFunction() thinks the function types are mismatched
and adds an extra bitcast, while the types are really the same.
This could also cause assertion failures at some places because with the
constexpr bitcast the Value is no longer a Funtion despite having a
FunctionType.

Replace StructType::create() with StructType::get().

Fixes: 30c0b679f459 ("entry code insertion v0")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

llvm/lib/Transforms/InnerUnikernels/IUInsertEntry.cpp
@@ -240,7 +240,7 @@ bool IUEntryInsertion::runOnModule(Module &M) {
       auto *ProgSelfTy = ProgFuncTy->getParamType(0);
 
       SmallVector<Type *, 0> CtxTys;
-      auto *CtxPT = StructType::create(C, CtxTys)->getPointerTo();
+      auto *CtxPT = StructType::get(C, CtxTys)->getPointerTo();
 
       Type *ProgRunArgTys[2] = {ProgSelfTy, CtxPT};
       auto *ProgRunRetty = Type::getInt32Ty(C);