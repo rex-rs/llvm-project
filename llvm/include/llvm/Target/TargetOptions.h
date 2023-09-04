8bc6118ec6f62c44d182fe1897fdce6828afaaa5
Jinghao Jia <jinghao7@illinois.edu>
2023-09-04 16:41:00 -0500

    Restrict stack usage per function frame
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- llvm/include/llvm/Target/TargetOptions.h
+++ llvm/include/llvm/Target/TargetOptions.h
@@ -385,6 +385,9 @@ namespace llvm {
     /// (lack) these extensions.
     unsigned VerifyArgABICompliance : 1;
 
+    /// Inner-unikernel flag
+    unsigned IUEnabled : 1;
+
     /// Name of the stack usage file (i.e., .su file) if user passes
     /// -fstack-usage. If empty, it can be implied that -fstack-usage is not
     /// passed on the command line.
