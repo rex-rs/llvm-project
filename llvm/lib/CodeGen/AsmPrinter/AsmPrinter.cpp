1eedc070a3c042fd748e1e7dfd966e394d969230
Jinghao Jia <jinghao7@illinois.edu>
2023-09-11 18:47:39 -0500

    Handle dynamically-sized stack
    抱きしめて 受け止めて
    悲しみも流した涙も いつか超えていける
    
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -1959,22 +1959,31 @@ void AsmPrinter::handleCallsiteForCallgraph(
 }
 
 void AsmPrinter::checkStackUsageIU(const MachineFunction &MF) const {
-  unsigned PageSize = 0x1000;
+  unsigned Limit = 0x1000;
+  const MachineFrameInfo &FrameInfo = MF.getFrameInfo();
 
   // ICE if threshold exceeded
-  if (MF.getFrameInfo().getStackSize() >= PageSize) {
+  if (FrameInfo.getStackSize() >= Limit) {
     std::string ErrMsg;
     {
       raw_string_ostream OS(ErrMsg);
-      OS << "Stack usage exceeded 1 page (4096 bytes)"
+      OS << "Stack usage exceeded limit of 4096 bytes"
+         << " for function " << MF.getName()
+         << " in inner-unikernel module "
+         << MF.getFunction().getParent()->getName();
+    }
+    report_fatal_error(StringRef(ErrMsg));
+  } else if (FrameInfo.hasVarSizedObjects()) {
+    std::string ErrMsg;
+    {
+      raw_string_ostream OS(ErrMsg);
+      OS << "Stack contains variable sized objects"
          << " for function " << MF.getName()
          << " in inner-unikernel module "
          << MF.getFunction().getParent()->getName();
     }
     report_fatal_error(StringRef(ErrMsg));
   }
-
-  // TODO: Handle dynamic stack size
 }
 
 /// EmitFunctionBody - This method emits the body and trailer for a
