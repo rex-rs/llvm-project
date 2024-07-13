ca8641de266b304a020cc7301f6b4e3ab9fb0fb9
Jinghao Jia <jinghao7@illinois.edu>
2024-07-13 02:33:05 -0500

    migrate remaining 'iu' names to 'rex'
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- llvm/lib/Target/X86/X86.h
+++ llvm/lib/Target/X86/X86.h
@@ -170,7 +170,7 @@ FunctionPass *createX86LoadValueInjectionRetHardeningPass();
 FunctionPass *createX86SpeculativeLoadHardeningPass();
 FunctionPass *createX86SpeculativeExecutionSideEffectSuppression();
 FunctionPass *createX86ArgumentStackSlotPass();
-ModulePass *createX86IUFrameSizePass();
+ModulePass *createX86RexFrameSizePass();
 
 void initializeCompressEVEXPassPass(PassRegistry &);
 void initializeFPSPass(PassRegistry &);
@@ -206,7 +206,7 @@ void initializeX86ReturnThunksPass(PassRegistry &);
 void initializeX86SpeculativeExecutionSideEffectSuppressionPass(PassRegistry &);
 void initializeX86SpeculativeLoadHardeningPassPass(PassRegistry &);
 void initializeX86TileConfigPass(PassRegistry &);
-void initializeX86IUFrameSizePassPass(PassRegistry &);
+void initializeX86RexFrameSizePassPass(PassRegistry &);
 
 namespace X86AS {
 enum : unsigned {
