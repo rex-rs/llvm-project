3d088f201560a7f729f62965fc1a4b7cd6b01dfa
Jinghao Jia <jinghao7@illinois.edu>
2024-07-13 02:33:05 -0500

    migrate remaining 'iu' names to 'rex'
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -1674,7 +1674,7 @@ static bool needFuncLabels(const MachineFunction &MF) {
       classifyEHPersonality(MF.getFunction().getPersonalityFn()));
 }
 
-void AsmPrinter::checkStackUsageIU(const MachineFunction &MF) const {
+void AsmPrinter::checkStackUsageRex(const MachineFunction &MF) const {
   unsigned Limit = 0x1000;
   const MachineFrameInfo &FrameInfo = MF.getFrameInfo();
 
@@ -1685,7 +1685,7 @@ void AsmPrinter::checkStackUsageIU(const MachineFunction &MF) const {
       raw_string_ostream OS(ErrMsg);
       OS << "Stack usage exceeded limit of 4096 bytes"
          << " for function " << MF.getName()
-         << " in inner-unikernel module "
+         << " in Rex module "
          << MF.getFunction().getParent()->getName();
     }
     report_fatal_error(StringRef(ErrMsg));
@@ -1695,7 +1695,7 @@ void AsmPrinter::checkStackUsageIU(const MachineFunction &MF) const {
       raw_string_ostream OS(ErrMsg);
       OS << "Stack contains variable sized objects"
          << " for function " << MF.getName()
-         << " in inner-unikernel module "
+         << " in Rex module "
          << MF.getFunction().getParent()->getName();
     }
     report_fatal_error(StringRef(ErrMsg));
@@ -2033,8 +2033,8 @@ void AsmPrinter::emitFunctionBody() {
   emitStackUsage(*MF);
 
   // Check the stack usage for inner-unikernel programs
-  if (MF->getTarget().Options.IUEnabled)
-    checkStackUsageIU(*MF);
+  if (MF->getTarget().Options.RexEnabled)
+    checkStackUsageRex(*MF);
 
   emitPatchableFunctionEntries();
 
